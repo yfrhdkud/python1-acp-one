de234521050857747c22f40ab1cb21f97d8fbe21
EJA <eraajaswal@gmail.com>
2025-04-16 19:07:35 +0530

    Add files via upload

---
+++ number swapping.py
@@ -0,0 +1,13 @@
+x = 5
+y = 9
+z = 1
+
+values = [x, y, z]
+
+values.sort()
+
+x, y, z = values
+
+print("Value of x after sorting:", x)
+print("Value of y after sorting:", y)
+print("Value of z after sorting:", z)
