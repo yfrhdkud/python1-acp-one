47bf5b309911ebb4f8db0bf084d02858b95f8fef
EJA <eraajaswal@gmail.com>
2025-05-06 15:19:46 +0530

    Update number swapping.py

--- number swapping.py
+++ number swapping.py
@@ -1,12 +1,11 @@
-x = 5
-y = 9
-z = 1
+x = int(input("Enter the first number x: "))
+y = int(input("Enter the second number y: "))
+z = int(input("Enter the third number z: "))
 
-values = [x, y, z]
 
-values.sort()
-
-x, y, z = values
+temp = x
+x = y
+y = temp
 
 print("Value of x after sorting:", x)
 print("Value of y after sorting:", y)
